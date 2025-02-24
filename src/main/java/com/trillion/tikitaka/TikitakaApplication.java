fd707cc7e0210f0985bc14562c699bfed7ef893c
Hojun Im <ghwnsdla8094@gmail.com>
2025-02-25 01:33:02 +0900

    [TKT-5] chore: JWT 및 QueryDSL 의존성 추가

--- src/main/java/com/trillion/tikitaka/TikitakaApplication.java
+++ src/main/java/com/trillion/tikitaka/TikitakaApplication.java
@@ -1,12 +1,17 @@
 package com.trillion.tikitaka;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class TikitakaApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(TikitakaApplication.class, args);
 	}
 
